a031bdc8fcdc5df328b82d93cde38645cc45314f
Moonsta0111 <john714726@gmail.com>
2023-09-07 10:39:01 +0900

    Pyramid

--- Cpractice/main.cpp
+++ Cpractice/main.cpp
@@ -1,9 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello KNU!\n");
-    printf("Hello SW!\n");
+    int i , j, k, n = 0;
+
+    printf(" 피라미드를 몇 층으로 쌓을지 정하시오. : ");
+    scanf("%d", &n);
+
+    printf("\n%d층 피라미드\n", n);
+    
+    for (i = 0; i < n; i++)
+    {
+        for (j = i; j <= n - 1; j++) 
+        {
+            printf(" ");
+        }
+        for (k = 0; k <= (i * 2); k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
