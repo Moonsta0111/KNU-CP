c0f90d5666823bcb025e04eded9bc1c2213ac2f3
Moonsta0111 <john714726@gmail.com>
2023-09-07 11:49:27 +0900

    FIle locate change

--- Cpractice/main.cpp
+++ Cpractice/main.cpp
@@ -3,16 +3,18 @@
 
 int main()
 {
-    int i , j, k, n = 0;
+    printf("Hello KNU!\n");
+    printf("Hello SW!\n");
+    int i, j, k, n = 0;
 
     printf(" 피라미드를 몇 층으로 쌓을지 정하시오. : ");
     scanf("%d", &n);
 
     printf("\n%d층 피라미드\n", n);
-    
+
     for (i = 0; i < n; i++)
     {
-        for (j = i; j <= n - 1; j++) 
+        for (j = i; j <= n - 1; j++)
         {
             printf(" ");
         }
