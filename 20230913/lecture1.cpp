d6055b44c1f9803049614320aa0307092ffef0f9
Moonsta0111 <john714726@gmail.com>
2023-09-14 09:38:45 +0900

    printf scanf

---
+++ 20230913/lecture1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("1.정수 입력: ");
+    scanf_s("%d", &num);
+    printf("2.키보드로 입력 받은 값은 %d입니다.\n", num);
+    return 0;
+}
