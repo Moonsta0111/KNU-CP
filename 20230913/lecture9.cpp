26d9e3ea1f1e09378412eca77011c5b75b9b4ea1
Moonsta0111 <john714726@gmail.com>
2023-09-14 12:05:07 +0900

    for sum

---
+++ 20230913/lecture9.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int sum = 0;
+    int val = 1;
+    for (i = 1; i < 10; i+=2) {
+        printf("i = %d, val = %d, sum = %d\n", i, val, sum);
+        sum += val;
+        val += 1;
+    }
+
+    printf("sum = %d\n", sum);
+    
+    return 0;
+}
