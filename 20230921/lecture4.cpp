36c50d8250486d5825449e85f20e9f075d1d790f
Moonsta0111 <john714726@gmail.com>
2023-10-06 18:50:43 +0900

    Add The loading bar

--- 20230921/lecture4.cpp
+++ 20230921/lecture4.cpp
@@ -9,12 +9,13 @@ int main() {
     double x = 0;
     double y = 0;
 
-    int count = 0, circle = 0;
+    int count = 1,full_square = 0;
+    int i, j;
     double of_circle = 0;
     double circum = 0;
     srand(time(NULL));
 
-    while (count < 1000000000) {
+    while (count <= 1000000000) {
         x = (double)rand() / (double)RAND_MAX;
         y = (double)rand() / (double)RAND_MAX;
 
@@ -27,7 +28,15 @@ int main() {
         circum = 4 * (of_circle / (float)count);
 
         if (count % 10000000 == 0) {
-            printf("%d%% 진행.. 원주율 : %lf\n", count / 10000000, circum);
+            printf("%3d%% 진행.. 원주율 : %lf  ", count / 10000000, circum);
+            full_square = (count * 0.00000002);
+            for (i = 0; i < full_square; i++) {
+                printf("■ ");
+            }
+            for (j = 0; j < (20 - full_square); j++) {
+                printf("□ ");
+            }
+            printf("\n");
         }
         count++;
     }
